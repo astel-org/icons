9344637aeee0b5fe37430ea843159d793fa31137
sumitkolhe <thesumitkolhe@gmail.com>
2022-05-01 21:36:13 +0530

    feat: minify

--- astel.config.ts
+++ astel.config.ts
@@ -2,14 +2,14 @@ import { defineAstelConfig } from '@astel/cli'
 
 export default defineAstelConfig({
   ignoreDirs: ['.DS_Store'],
+  prefix: '',
+  declarationDir: 'dist/types',
   build: {
     entry: 'packages/index.ts',
-    prefix: '',
     emptyOutDir: true,
-    declaration: true,
     library: [
-      { name: 'astel', format: 'esm', outputDir: 'dist/es', bundle: false, summary: true },
-      { name: 'astel', format: 'umd', outputDir: 'dist/lib', bundle: false, summary: true },
+      { format: 'esm', outputDir: 'dist/es', summary: true, minify: true },
+      { format: 'umd', outputDir: 'dist/lib', summary: true, minify: true },
     ],
   },
 })
